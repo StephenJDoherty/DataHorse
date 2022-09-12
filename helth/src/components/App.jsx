6e305eb8f603f9bfcb34338878d553b7a66875a3
Stephen Doherty <64056028+StephenJDoherty@users.noreply.github.com>
2022-09-12 19:42:30 -0400

    getting app kicked off

--- helth/src/components/App.jsx
+++ helth/src/components/App.jsx
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// JOIN AUDIO IF U WANT LOL
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello DataHorse Team!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
