8df540588f3f3b0530cb2fd9dceeceac6637e7b6
Stephen Doherty <64056028+StephenJDoherty@users.noreply.github.com>
2022-10-03 22:38:15 -0400

    adding calendar and router

---
+++ helth/src/pages/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import BigCalendar from "./Calendar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Calendar" element={<BigCalendar />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
