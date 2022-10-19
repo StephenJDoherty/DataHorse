b4dcc293b34a445d1e8d69f48be58fcf707670ba
Stephen Doherty <64056028+StephenJDoherty@users.noreply.github.com>
2022-10-19 19:43:24 -0400

    chore

--- helth/src/components/NavBar.jsx
+++ helth/src/components/NavBar.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Calendar">Calendar</Link>
-          </li>
-          <li>
-            <Link to="/HabitPage">My Habits</Link>
-          </li>
-        </ul>
-      </nav>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Calendar">Calendar</Link>
+        </li>
+        <li>
+          <Link to="/HabitPage">My Habits</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
