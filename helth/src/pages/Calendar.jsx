326d223993b7d849ead91c2568231ad9e3275dec
Stephen Doherty <64056028+StephenJDoherty@users.noreply.github.com>
2022-11-27 13:58:14 -0500

    chore: remove comments

--- helth/src/pages/Calendar.jsx
+++ helth/src/pages/Calendar.jsx
@@ -1,27 +1,13 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import {
-  collection,
-  doc,
-  getDocs,
-  getFirestore,
-  query,
-  setDoc,
-  where,
-} from "firebase/firestore";
-import { auth } from "../firebase";
 
 import "../App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
-const db = getFirestore();
-
 const MyCalendar = (props) => {
-  // can do a map to myEvents for all the stuff in historyNice based on the date
-
   const [myEvents, setMyEvents] = useState([
     {
       start: "",
@@ -30,46 +16,6 @@ const MyCalendar = (props) => {
       mood: "",
     },
   ]);
-
-  // connect to the database and pull data into the calendar based
-  // on the dates that have habit history
-
-  // useEffect(() => {
-  //   console.log("here 2");
-  //   const uid = auth.currentUser.uid;
-  //   // run one time, when page is rendered
-  //   // want to getDoc for the historyNice table and
-  //   // for the rating 1-5 show the smiling face emoji associated, also
-  //   // make the comment green if 5/4, yellow if 3/2 and red if 1 (5 highest)
-  //   // need the historyNice to include the date when set
-  //   // question what is a score good/bad?
-
-  //   const today = new Date(); // format this to match what we are saving
-  //   const scoreMood = collection(db, "emoji");
-  //   const query = query(scoreMood, where("uid", "==", uid));
-
-  //   // create the object array where the date matches and then call SetMyEvents
-  //   // map thru the events and create an object with start, end, and score/ mood emoji
-
-  //   const dailyRating = getDocs(query);
-  //   const eventHistory = [];
-  //   dailyRating.forEach((doc) => {
-  //     console.log("here 3");
-  //     let mood = doc.get("mood");
-  //     let date = doc.get("date");
-  //     let dateMoment = moment(date);
-  //     // push this mood eventhistory array
-  //     eventHistory.push({
-  //       start: dateMoment,
-  //       end: moment().add(1, "days").toDate(),
-  //       title: mood,
-  //     });
-  //   });
-
-  //   setMyEvents(eventHistory);
-  //   console.log("My eventhistory: ", eventHistory);
-  //   console.log("my events: ", myEvents);
-  // }, []);
 
   const handleSelectSlot = useCallback(
     ({ start, end }) => {
@@ -93,16 +39,7 @@ const MyCalendar = (props) => {
   };
 
   const eventStyleGetter = () => {
-    let backgroundColor = "blue";
-    let score = ""; // need to get the score locally here
-    // update these scores to match whats desired
-    if (score > 50) {
-      backgroundColor = "red";
-    } else if (score < 50) {
-      backgroundColor = "yellow";
-    } else {
-      backgroundColor = "green";
-    }
+    let backgroundColor = "lightblue";
     let style = {
       backgroundColor: backgroundColor,
     };
